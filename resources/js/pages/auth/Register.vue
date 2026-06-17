fdfc1ae631f0553ebbe5578673455091b9d53862
Alex Cutler <calexcutler@gmail.com>
2026-06-17 11:04:32 -0600

    Updated code styling

--- resources/js/pages/auth/Register.vue
+++ resources/js/pages/auth/Register.vue
@@ -9,130 +9,61 @@ import { Label } from '@/components/ui/label';
 import { Spinner } from '@/components/ui/spinner';
 import { login } from '@/routes';
 import { store } from '@/routes/register';
-
 defineOptions({
     layout: {
         title: 'Create an account',
-        description: 'Enter your details below to create your account',
-    },
+        description: 'Enter your details below to create your account'
+    }
 });
-
-const form = useForm({
-    name: '',
-    username: '',
-    email: '',
-    phone_number: '',
-    password: '',
-    password_confirmation: '',
-});
-
+const form = useForm({name: '', username: '', email: '', phone_number: '', password: '', password_confirmation: ''});
 function submit() {
     form.submit(store().method, store().url, {
         onSuccess: () => form.reset('password', 'password_confirmation')
     });
 }
 </script>
-
 <template>
-    <Head title="Register" />
-
+    <Head title="Register"/>
     <form @submit.prevent="submit" class="flex flex-col gap-6">
         <div class="grid gap-6">
             <div class="grid gap-2">
                 <Label for="name">Name</Label>
-                <Input
-                    id="name"
-                    type="text"
-                    required
-                    autofocus
-                    :tabindex="1"
-                    autocomplete="name"
-                    v-model="form.name"
-                    placeholder="Full name"
-                />
-                <InputError :message="form.errors.name" />
+                <Input id="name" type="text" required autofocus :tabindex="1" autocomplete="name" v-model="form.name" placeholder="Full name"/>
+                <InputError :message="form.errors.name"/>
             </div>
-
             <div class="grid gap-2">
                 <Label for="username">Username</Label>
-                <Input
-                    id="username"
-                    type="text"
-                    required
-                    :tabindex="2"
-                    autocomplete="username"
-                    v-model="form.username"
-                    placeholder="username"
-                />
-                <InputError :message="form.errors.username" />
+                <Input id="username" type="text" required :tabindex="2" autocomplete="username" v-model="form.username" placeholder="username"/>
+                <InputError :message="form.errors.username"/>
             </div>
-
             <div class="grid gap-2">
                 <Label for="email">Email address</Label>
-                <Input
-                    id="email"
-                    type="email"
-                    required
-                    :tabindex="3"
-                    autocomplete="email"
-                    v-model="form.email"
-                    placeholder="email@example.com"
-                />
-                <InputError :message="form.errors.email" />
+                <Input id="email" type="email" required :tabindex="3" autocomplete="email" v-model="form.email" placeholder="email@example.com"/>
+                <InputError :message="form.errors.email"/>
             </div>
-
             <div class="grid gap-2">
                 <Label for="phone_number">Phone Number</Label>
                 <Input id="phone_number" type="tel" :tabindex="4" autocomplete="tel" v-model="form.phone_number" placeholder="+1 (555) 000-0000"/>
                 <InputError :message="form.errors.phone_number"/>
             </div>
-
             <div class="grid gap-2">
                 <Label for="password">Password</Label>
-                <PasswordInput
-                    id="password"
-                    required
-                    :tabindex="5"
-                    autocomplete="new-password"
-                    v-model="form.password"
-                    placeholder="Password"
-                />
-                <InputError :message="form.errors.password" />
+                <PasswordInput id="password" required :tabindex="5" autocomplete="new-password" v-model="form.password" placeholder="Password"/>
+                <InputError :message="form.errors.password"/>
             </div>
-
             <div class="grid gap-2">
                 <Label for="password_confirmation">Confirm password</Label>
-                <PasswordInput
-                    id="password_confirmation"
-                    required
-                    :tabindex="6"
-                    autocomplete="new-password"
-                    v-model="form.password_confirmation"
-                    placeholder="Confirm password"
-                />
-                <InputError :message="form.errors.password_confirmation" />
+                <PasswordInput id="password_confirmation" required :tabindex="6" autocomplete="new-password" v-model="form.password_confirmation" placeholder="Confirm password"/>
+                <InputError :message="form.errors.password_confirmation"/>
             </div>
-
-            <Button
-                type="submit"
-                class="mt-2 w-full"
-                tabindex="7"
-                :disabled="form.processing"
-                data-test="register-user-button"
-            >
+            <Button type="submit" class="mt-2 w-full" tabindex="7" :disabled="form.processing" data-test="register-user-button">
                 <Spinner v-if="form.processing" />
                 Create account
             </Button>
         </div>
-
         <div class="text-center text-sm text-muted-foreground">
             Already have an account?
-            <TextLink
-                :href="login()"
-                class="underline underline-offset-4"
-                :tabindex="8"
-                >Log in</TextLink
-            >
+            <TextLink :href="login()" class="underline underline-offset-4" :tabindex="8">Log in</TextLink>
         </div>
     </form>
 </template>
