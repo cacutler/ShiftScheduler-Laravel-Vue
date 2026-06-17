fdda25ef338e0442bb0322e65791319ff4d51b6e
Alex Cutler <calexcutler@gmail.com>
2026-06-17 11:41:01 -0600

    Updated the requests related to storing and updating users

--- app/Http/Requests/UserStoreRequest.php
+++ app/Http/Requests/UserStoreRequest.php
@@ -8,11 +8,12 @@ use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 class UserStoreRequest extends FormRequest {
+    use PasswordValidationRules, ProfileValidationRules;
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool {
-        return false;
+        return $this->user()->can("create", User::class);
     }
     /**
      * Get the validation rules that apply to the request.
@@ -20,7 +21,11 @@ class UserStoreRequest extends FormRequest {
      */
     public function rules(): array {
         return [
-            //
+            ...$this->profileRules(),
+            "username" => ["required", "string", "max:255", Rule::unique(User::class)],
+            "phone_number" => ["nullable", "string", "max:20"],
+            "password" => $this->passwordRules(),
+            "status" => ["nullable", Rule::enum(UserStatus::class)]
         ];
     }
 }
--- app/Http/Requests/UserUpdateRequest.php
+++ app/Http/Requests/UserUpdateRequest.php
@@ -7,19 +7,24 @@ use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 class UserUpdateRequest extends FormRequest {
+    use ProfileValidationRules;
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool {
-        return false;
+        return true;
     }
     /**
      * Get the validation rules that apply to the request.
      * @return array<string, ValidationRule|array<mixed>|string>
      */
     public function rules(): array {
+        $userId = $this->route("user")->id;
         return [
-            //
+            ...$this->profileRules($userId),
+            "username" => ["required", "string", "max:255", Rule::unique(User::class)->ignore($userId)],
+            "phone_number" => ["nullable", "string", "max:20"],
+            "status" => ["nullable", Rule::enum(UserStatus::class)]
         ];
     }
 }
